f43600f76569bd6ed34c6cd3e88b8430a677f687
vamshikrishnakodem95 <vamshikdm@gmail.com>
2023-11-09 12:20:41 +0530

    implemented the infinite scroll 2 methods

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import { GridLights } from "./pages/gridLights/gridLights";
 import { Pagination } from "./pages/pagination/pagination";
+import { InfiniteScroll } from "./pages/infiniteScroll/infiniteScroll";
 
 const App = () => {
   return (
     <div className="App">
       {/* <GridLights /> */}
-      <Pagination />
+      {/* <Pagination /> */}
+      <InfiniteScroll />
     </div>
   );
 };
---
+++ src/pages/infiniteScroll/infiniteScroll.jsx
@@ -0,0 +1,31 @@
+import React, { useEffect, useState, useRef, useCallback } from "react";
+
+import "./infiniteScroll.css";
+import { InfiniteScrollCard } from "./infiniteScrollCard";
+import { useInfiniteScroll } from "./useInfiniteScroll";
+
+export const InfiniteScroll = () => {
+  const { products, lastCardList } = useInfiniteScroll();
+
+  return (
+    <div className="infinity-container">
+      <h1>Infinity Scroll Project</h1>
+      <div className="products-container">
+        {products.map((product, index) => {
+          if (products.length === index + 1) {
+            return (
+              <InfiniteScrollCard
+                lastCardRef={lastCardList}
+                product={product}
+                key={product.id + index}
+              />
+            );
+          }
+          return (
+            <InfiniteScrollCard product={product} key={product.id + index} />
+          );
+        })}
+      </div>
+    </div>
+  );
+};

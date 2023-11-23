913aed85ca229b1bd97cbefa7341e61567072325
vamshikrishnakodem95 <vamshikdm@gmail.com>
2023-11-23 19:42:11 +0530

    added custom hook for local storage

---
+++ src/customHooks/useLocalStorage/useLocalStorage.js
@@ -0,0 +1,41 @@
+export const useLocalStorage = (key) => {
+  const getItem = () => {
+    try {
+      const result = window.localStorage.getItem(key);
+      return result ? JSON.parse(result) : undefined;
+    } catch (err) {
+      console.log("error getting the item", err);
+    }
+  };
+
+  const setItem = (value) => {
+    try {
+      localStorage.setItem(key, JSON.stringify(value));
+    } catch (err) {
+      console.log("error setting the item", err);
+    }
+  };
+
+  const removeItem = () => {
+    try {
+      localStorage.removeItem(key);
+    } catch (err) {
+      console.log("error clearing the item", err);
+    }
+  };
+
+  const removeAll = () => {
+    try {
+      localStorage.clear();
+    } catch (err) {
+      console.log("error clearing local storage", err);
+    }
+  };
+
+  return {
+    getItem,
+    removeItem,
+    removeAll,
+    setItem,
+  };
+};

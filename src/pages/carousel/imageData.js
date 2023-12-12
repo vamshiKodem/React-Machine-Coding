f93f9203d3fc031ddf2722917e473422f15678b7
vamshikrishnakodem95 <vamshikdm@gmail.com>
2023-12-12 12:11:40 +0530

    completed the changes for carousel

---
+++ src/pages/carousel/imageData.js
@@ -0,0 +1,6 @@
+export const imageData = [
+  "https://cdn.pixabay.com/photo/2015/03/17/02/01/cubes-677092_1280.png",
+  "https://www.shutterstock.com/shutterstock/photos/2274412231/display_1500/stock-vector-many-rainbow-gradient-random-bright-soft-balls-background-colorful-balls-background-for-kids-zone-2274412231.jpg",
+  "https://miro.medium.com/v2/resize:fit:1400/1*M7_GzV-glZaZLNuSiNdiPQ.png",
+  "https://www.thedesignwork.com/wp-content/uploads/2011/10/Random-Pictures-of-Conceptual-and-Creative-Ideas-01.jpg",
+];

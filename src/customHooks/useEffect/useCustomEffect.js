26f57f68543ef9294338938397fd7e56c39516ed
vamshikrishnakodem95 <vamshikdm@gmail.com>
2023-12-20 12:03:18 +0530

    update dependency

--- src/customHooks/useEffect/useCustomEffect.js
+++ src/customHooks/useEffect/useCustomEffect.js
@@ -1,12 +1,21 @@
 import { useRef } from "react";
 
-export const useCustomEffect = (callback, dependencyArray = []) => {
+export const useCustomEffect = (callback, dependencyArray) => {
   const isFirstRender = useRef(true);
-  const dependencyRef = useRef(dependencyArray);
+  const prevDependencyRef = useRef([]);
 
   if (isFirstRender.current) {
     callback();
     isFirstRender.current = false;
     return;
   }
+
+  const isDependencyChanged =
+    JSON.stringify(prevDependencyRef) === JSON.stringify(dependencyArray);
+
+  if (isDependencyChanged) {
+    callback();
+  }
+
+  prevDependencyRef.current = dependencyArray || [];
 };

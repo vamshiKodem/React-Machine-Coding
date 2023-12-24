cdbb7419b2327d546a03b820aa4d8ee3acfc97ef
vamshikrishnakodem95 <vamshikdm@gmail.com>
2023-12-24 13:05:42 +0530

    adding invaild page and make jsx

--- src/pages/dynamicRouter/dynamicRouter.jsx
+++ src/pages/dynamicRouter/dynamicRouter.jsx
@@ -3,14 +3,16 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { About } from "./screens/about";
 import { Home } from "./screens/home";
 import { User } from "./screens/user";
+import { Invalid } from "./screens/invalid";
 
 export const DynamicRouter = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" Component={Home} />
+        <Route exact path="/" Component={Home} />
         <Route path="/about" Component={About} />
-        <Route path="/:userId" Component={User} />
+        <Route path="user/:userId" Component={User} />
+        <Route path="*" Component={Invalid} />
       </Routes>
     </BrowserRouter>
   );

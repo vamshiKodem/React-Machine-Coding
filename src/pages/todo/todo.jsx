e934777492314fc17c90071911f463bfb504ae0f
vamshiKodem <vamshikrishna.kodem96@gmail.com>
2024-05-26 14:11:05 +0530

    implemented add and delete todo

--- src/pages/todo/todo.jsx
+++ src/pages/todo/todo.jsx
@@ -3,19 +3,40 @@ import "./todo.css";
 import { useTodo } from "./useTodo";
 
 export const Todo = () => {
-  const { todoList, onCancelClick } = useTodo();
+  const {
+    todoList,
+    onCancelClick,
+    inputId,
+    onTodoInputChange,
+    onSubmitPress,
+    todoInput,
+    onDeleteTodoClick,
+  } = useTodo();
 
   return (
     <div className="todo-container">
+      <input
+        type="text"
+        placeholder="Add Todo"
+        id={inputId}
+        value={todoInput}
+        onChange={(e) => onTodoInputChange(e)}
+      />
+      <button onClick={onSubmitPress}>Submit</button>
       <ul>
         {todoList.map((list) => (
           <li key={list.id}>
             <h6 className={list.completed === true ? "completed" : null}>
               {list.todo}
             </h6>
-            <span className="link" onClick={() => onCancelClick(list.id)}>
-              X
-            </span>
+            <div>
+              <span className="link" onClick={() => onCancelClick(list.id)}>
+                Completed
+              </span>
+              <span className="link" onClick={() => onDeleteTodoClick(list.id)}>
+                Delete
+              </span>
+            </div>
           </li>
         ))}
       </ul>
--- src/pages/todo/useTodo.js
+++ src/pages/todo/useTodo.js
@@ -1,14 +1,18 @@
-import { useEffect, useState } from "react";
+import { useEffect, useId, useState } from "react";
 
 const GET_TODO = "https://dummyjson.com/todos";
 
 export const useTodo = () => {
   const [todoList, setTodoList] = useState([]);
+  const inputId = useId();
+  const [todoInput, setTodoInput] = useState("");
+
   const getTodo = async () => {
     try {
       const data = await fetch(GET_TODO);
       const jsonData = await data.json();
       setTodoList(jsonData.todos);
+      console.log(jsonData.todos);
     } catch (err) {
       console.log(err);
     }
@@ -31,8 +35,35 @@ export const useTodo = () => {
     setTodoList(updatedTodoList);
   };
 
+  const onTodoInputChange = (e) => {
+    setTodoInput(e.target.value);
+  };
+
+  const onSubmitPress = () => {
+    if (!todoInput) {
+      return;
+    }
+    const newTodo = {
+      completed: false,
+      id: todoList.length + 1,
+      todo: todoInput,
+    };
+    setTodoList([...todoList, newTodo]);
+    setTodoInput("");
+  };
+
+  const onDeleteTodoClick = (id) => {
+    const updatedTodos = todoList.filter((todo) => todo.id !== id);
+    setTodoList(updatedTodos);
+  };
+
   return {
     todoList,
     onCancelClick,
+    inputId,
+    todoInput,
+    onTodoInputChange,
+    onSubmitPress,
+    onDeleteTodoClick,
   };
 };

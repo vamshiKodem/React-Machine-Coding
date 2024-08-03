9d2344ff2d6e59178a5891cca2530254e9d8a2ac
vamshiKodem <vamshikrishna.kodem96@gmail.com>
2024-08-03 19:43:35 +0530

    added all logic for paginatio

--- src/pagination2/pagination2.jsx
+++ src/pagination2/pagination2.jsx
@@ -4,7 +4,44 @@ import { usePagination2 } from "./usePagination2";
 import "./pagination2.css";
 
 export const Pagination2 = () => {
-  const {} = usePagination2();
+  const {
+    data,
+    selectedPage,
+    onNextClick,
+    onPreviousClick,
+    numericButtons,
+    onNumericButtonClick,
+  } = usePagination2();
 
-  return <></>;
+  return (
+    <div className="pagination-container">
+      {data.slice((selectedPage - 1) * 10, selectedPage * 10).map((data) => (
+        <div key={data.title} className="list-container">
+          <h2>{data.title}</h2>
+          <p>{data.body}</p>
+        </div>
+      ))}
+
+      <div className="button-container">
+        {selectedPage === 1 ? null : (
+          <button onClick={onPreviousClick}>Previous</button>
+        )}
+        {numericButtons.map((val) => (
+          <button
+            className={`numeric-btn ${
+              selectedPage === val ? "selected" : null
+            }`}
+            onClick={() => onNumericButtonClick(val)}
+            key={val}
+            disabled={selectedPage === val}
+          >
+            {val}
+          </button>
+        ))}
+        {selectedPage === numericButtons.length ? null : (
+          <button onClick={onNextClick}>Next</button>
+        )}
+      </div>
+    </div>
+  );
 };
--- src/pagination2/usePagination2.js
+++ src/pagination2/usePagination2.js
@@ -1,3 +1,35 @@
+import { useCallback, useMemo, useState } from "react";
+
+import { mockData } from "./mockData";
+
 export const usePagination2 = () => {
-  return {};
+  const [data] = useState(mockData);
+  const [selectedPage, setSelectedPage] = useState(1);
+
+  const onNextClick = useCallback(() => {
+    setSelectedPage((prev) => (prev === data.length / 10 ? prev : prev + 1));
+  }, [data.length]);
+
+  const onPreviousClick = useCallback(() => {
+    setSelectedPage((prev) => (prev === 1 ? 1 : prev - 1));
+  }, []);
+
+  const onNumericButtonClick = (val) => {
+    setSelectedPage(val);
+  };
+
+  const numericButtons = useMemo(() => {
+    return Array(mockData.length / 10)
+      .fill()
+      .map((_, index) => index + 1);
+  }, []);
+
+  return {
+    data,
+    selectedPage,
+    onNextClick,
+    onPreviousClick,
+    numericButtons,
+    onNumericButtonClick,
+  };
 };

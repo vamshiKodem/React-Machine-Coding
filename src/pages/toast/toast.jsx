6358be944b5be0cb1ba13f61b4602052f2698c59
vamshiKodem <vamshikrishna.kodem96@gmail.com>
2024-08-25 18:02:55 +0530

    implemented the basic toast

--- src/pages/toast/toast.jsx
+++ src/pages/toast/toast.jsx
@@ -1,7 +1,36 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 
 import "./toast.css";
 
 export const Toast = () => {
-  return <h1>Toast</h1>;
+  const [showToast, setShowToast] = useState(false);
+  const toastRef = useRef(null);
+
+  const onButtonClick = () => {
+    setShowToast(!showToast);
+
+    toastRef.current = setTimeout(() => {
+      setShowToast(false);
+    }, 5000);
+  };
+
+  const onCancelClick = () => {
+    setShowToast(false);
+    clearTimeout(toastRef.current);
+  };
+
+  return (
+    <div className="toast-container">
+      {showToast ? (
+        <div className="toast">
+          <h2>Some Text</h2>
+
+          <button className="cancel-button" onClick={onCancelClick}>
+            X
+          </button>
+        </div>
+      ) : null}
+      <button onClick={onButtonClick}>Click Me</button>
+    </div>
+  );
 };
